19057c8ef4168bcfef418aad515ee2a22f299926
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-04-01 09:44:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ camera=document.getElementById("camera");
          document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'>";
      });
  }
+ console.log("ml5 version - ",ml5.version);
+ classifier=ml5.imageClassifier("",modleLoaded);
+ function modleLoaded(){
+     console.log("model loaded true");
+ }
+ var prediction1="";
+ var prediction2="";
+ function speak(){
+     synth=window.speechSynthesis;
+     speak_data1="the first prediction is- ",prediction1;
+     speak_data2="and the second prediction is- ",prediction2;
+     uttethis=new SpeechSynthesisUtterance(speak_data1 , speak_data2);
+     synth.speak(uttethis);
+ }
+ function check(){
+     img=document.getElementById("captured_image");
+     classifier.classify(img ,gotResult);
+ }
