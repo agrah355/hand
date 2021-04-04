8b9d439ea33e665ca5a48b569ab20f22b08dc877
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-04-04 11:49:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,18 +7,17 @@ Webcam.set({
 camera=document.getElementById("camera");
  Webcam.attach("#camera");
 
+ console.log("ml5 version :",ml5.version);
  function takeSnapshot(){
-     Webcam.snap(function(data_uri){
-         document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'>";
-     });
- }
- console.log("ml5 version - ",ml5.version);
- classifier=ml5.imageClassifier("",modleLoaded);
- function modleLoaded(){
-     console.log("model loaded true");
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+ classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Aoe6W_KiP/mode.json",modelLoaded);
+ function modelLoaded(){
+     console.log("model loadded true");
  }
  var prediction1="";
- var prediction2="";
  function speak(){
      synth=window.speechSynthesis;
      speak_data1="the first prediction is- ",prediction1;
@@ -29,4 +28,23 @@ camera=document.getElementById("camera");
  function check(){
      img=document.getElementById("captured_image");
      classifier.classify(img ,gotResult);
+ }
+ function gotResult(error,results){
+     if(error){
+         console.error(error);
+     }else{
+         console.log(results);
+         document.getElementById("name_pose_name").innerHTML=results[0].label;
+        prediction1=results[0].label;
+        speak();
+        if(prediction1=="thumb up"){
+            document.getElementById("update_emoji").innerHTML="&#128077;";
+        }
+        if(prediction1=="thumb down"){
+            document.getElementById("update_emoji").innerHTML="&#128078;";
+        }
+        if(prediction1=="xy"){
+            document.getElementById("update_emoji").innerHTML="&#128079;"; 
+        }
+     }
  }
